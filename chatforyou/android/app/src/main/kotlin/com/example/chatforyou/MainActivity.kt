9644e88979b3a5972875da50641b962bca8fb7ac
Nandini1O1 <nandinikashyap101@gmail.com>
2022-06-10 00:15:56 +0530

    Initial commit

---
+++ chatforyou/android/app/src/main/kotlin/com/example/chatforyou/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chatforyou
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ chatforyou/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'Screens/welcome_screen.dart';
+import 'Screens/login_screen.dart';
+import 'Screens/registration_screen.dart';
+import 'Screens/chat_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const FlashChat());
+}
+// void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  const FlashChat({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => const WelcomeScreen(),
+        LoginScreen.id: (context) => const LoginScreen(),
+        RegistrationScreen.id: (context) => const RegistrationScreen(),
+        ChatScreen.id: (context) => const ChatScreen(),
+      },
+      home: const WelcomeScreen(),
+    );
+  }
+}
